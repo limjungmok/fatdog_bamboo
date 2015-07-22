5a4ef758b8fd8b96f7bfe22d0a553170ea584514
JM <limjungmok@gmail.com>
2015-07-22 16:03:16 +0900

    Home css JM

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,20 @@
 class PagesController < ApplicationController
   def home
+  	@boards = Board.all
   end
 
   def board
   end
 
-  def write
-  	
+  def write	
   end
+
+
+  def getSlideBoard
+  	@boards = Board.all
+  	@slide_board = boards.find(params[:click_count])
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   get 'write' =>  'pages#write'
   get 'board' =>  'pages#board'
   
-
 end
