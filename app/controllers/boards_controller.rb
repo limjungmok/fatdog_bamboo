ae76c724a1a61c9b6e93b688cd518c70b87024e1
coco <jws1784@gmail.com>
2015-07-23 21:54:32 +0900

    20150723 sj_x complete

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -12,7 +12,15 @@ class BoardsController < ApplicationController
 	end
 
 	def index
-		@boards = Board.all
+		if(params[:b_category])
+			@boards = Board.where("b_category = ? ", params[:b_category])
+		elsif(params[:b_click_count])
+			@boards = Board.order("b_click_count DESC")
+		elsif(params[:b_like])
+			@boards = Board.order("b_like DESC")
+		else
+			@boards = Board.all
+		end
 	end
 
 	def show
@@ -28,13 +36,14 @@ class BoardsController < ApplicationController
 	end
 
 	def destroy
-		
+		Board.find(params[:id]).destroy
+		redirect_to boards_path
 	end
 
 
 	private
 
 	def board_params
-		params.require(:board).permit(:b_category, :b_content)
+		params.require(:board).permit(:b_category, :b_content, :b_click_count, :b_like)
 	end
 end
