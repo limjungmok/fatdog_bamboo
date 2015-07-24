b3142a92760611cbd02001645cc01664d1a44304
JM <limjungmok@gmail.com>
2015-07-24 17:09:30 +0900

    20150724 session admin complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
+  root 'pages#home'
+
   get 'login'		=> 'sessions#new'
   post 'login'		=> 'sessions#create'
   delete 'logout'	=> 'sessions#destroy'
-
-  root 'pages#home'
   
   resources :boards
   resources :users
