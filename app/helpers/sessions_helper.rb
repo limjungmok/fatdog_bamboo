39b33e233b99895088bf40a3ff862275c875d044
JM <limjungmok@gmail.com>
2015-07-28 17:06:14 +0900

    jm

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,23 +1,33 @@
 module SessionsHelper
 
-	def log_in(user)
-		session[:user_id] = user.id 
-	end
+  def log_in(user)
+	session[:user_id] = user.id 
+  end
 
-	def current_user
-		if @current_user.nil?
-			@current_user = User.find_by(id: session[:user_id])
-		else
-			@current_user
-		end
-	end
+  def current_user?(user)
+    user == current_user
+  end
 
-	def logged_in?
-		!current_user.nil?
-	end
+ # Returns the user corresponding to the remember token cookie.
+  def current_user
+	if (user_id = session[:user_id])
+  		@current_user ||= User.find_by(id: user_id)
+	elsif (user_id = cookies.signed[:user_id])
+  		user = User.find_by(id: user_id)
+    	log_in user
+    	@current_user = user
+    end
+  end
 
-	def log_out
-		session.delete(:user_id)
-		@current_user = nil
-	end
+  def logged_in?
+	!current_user.nil?
+  end
+
+  def log_out
+  	session.delete(:user_id) 
+	@current_user = nil
+  end
 end
+
+
+
