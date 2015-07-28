b3932524c7eb077a9a6148a66bde61a748dc922a
coco <jws1784@gmail.com>
2015-07-28 21:40:20 +0900

    git pull

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20150728071212) do
     t.boolean  "u_admin"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.string   "u_id"
     t.string   "password_digest"
   end
 
