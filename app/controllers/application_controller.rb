5220c4eb15db37b6a5fc7c5da6b71e4e0c151e0f
JM <limjungmok@gmail.com>
2015-08-04 11:09:33 +0900

    jm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+    def remote_ip
+    if request.remote_ip == '127.0.0.1'
+      # Hard coded remote address
+      '123.45.67.89'
+    else
+      request.remote_ip
+    end
+  end
 end
